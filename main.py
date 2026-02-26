9b9fddefab7336a380987458911cbc57fa4786c3
jhotton-envqc <julien.hotton@environnement.gouv.qc.ca>
2026-02-26 15:00:42 -0500

    update menu

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug 28 09:53:36 2025
-
 @author: hotju02
 """
-
 import streamlit as st
 
-#st.title("This is the title main page")
+st.markdown("# 🎧 décibel-utils")
 
-# Main page content
-st.markdown("# 🎧 decibel-utils")
+st.subheader("Outils d’analyse, de calcul et de visualisation pour des données acoustiques et météo")
 
 st.markdown(
-    "Cette page est un prototype...\n\n"
-    "Outils d’analyse, de calcul et de visualisation pour des données acoustiques et météo\n\n"
-    "pour info: julien.hotton@environnement.gouv.qc.ca"
+    """
+pour info: julien.hotton@environnement.gouv.qc.ca
+    """
 )
 
-#st.sidebar.markdown("# 🎈 Page d'accueil 🎈")
+st.divider()
+
+st.subheader("Aller directement à :")
+
+# Single column list of page links (no icons)
+st.page_link("page_2.py", label="Calculatrice décibels")
+st.page_link("page_3.py", label="Calculatrice Lden")
+st.page_link("page_4.py", label="Rose des vents")
+st.page_link("page_5.py", label="Multi-traces")
+
+
